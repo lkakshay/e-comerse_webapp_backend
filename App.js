84a333fd674f44ebbeae0bc385dad60f893535e5
lkakshay <connectme.lkakshay@gmail.com>
2022-08-15 23:21:08 +0530

    test

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 const express= require('express')
 const connect  = require("./src/configs/db/db")
 const app=express()
+require('dotenv').config()
 const port=process.env.PORT
 
 app.use(express.json())
