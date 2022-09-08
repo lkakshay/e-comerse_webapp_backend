41d2ffc174da5022927706fc292f2e44a0aeb1a3
akshaylk <connectme.lkakshay@gmail.com>
2022-09-08 12:24:56 +0530

    product controller  updated

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -13,12 +13,14 @@ router.get("/single", async (req, res) => {
 
 });
 router.get("", async (req, res) => {
-  const query ='J'
-  const page=1
- 
+  const query =req.query.category
+  const page=+(req.query.page)
+  const order=+(req.query.order)
+  const sort=req.query.sort
+  
   try {
-    NumberOfDocument = await Product.find({category:{$regex: `^${query}`}}).countDocuments().lean().exec()/16
-    document= await Product.find({category:{$regex: `^${query}`}}).skip((page-1)*16).limit(16)
+    NumberOfDocument = await Product.find({category:{$regex: `^${query}`,$options:'i'}}).sort({[sort]:order}).countDocuments().lean().exec()/16
+    document= await Product.find({category:{$regex: `^${query}`,$options:'i'}}).sort({[sort]:order}).skip((page-1)*16).limit(16)
     res.status(201).send({totalPages:Math.ceil(NumberOfDocument),data:document});
   } catch (error) {
     res.send(error.message);
